460413aebad799ea04c798c789af7ebc9af2e2f0
Augusto Estevao Monte <augusto-estevao@hotmail.com>
2022-03-12 12:09:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+<h1>Template de desenvolvimento Front-End</h1>
+<p>O seguinte template contém um arquivo Gulp.js que para a sua correta utilização é necessário utilizar alguns comandos, sendo eles:</p>
+<p><code>npm init</code></p>
+<p><code>npm install -g gulp-cli</code></p>
+<p><code>sudo npm install @babel/core @babel/preset-env postcss autoprefixer browser-sync cssnano dart-sass gulp gulp-babel gulp-postcss gulp-sass gulp-terser gulp-imagemin gulp-cache<code></p>
+<h2>Depois de executado estes comandos, a seguir devemos usar: </h2>
+<p><code>gulp</code></p>
+<br><br>
+<h2>Beneficios desse template</h2>
+<p>Live server integrado</p>
+<p>Limpa o cache do navegador a cada atualização feita nos arquivos</p>
+<p>Transpilação de todo código em SASS da pasta "app/scss" para a pasta "dist"</p>
+<p>Otimiza todas as imagens dentro da pasta "app/images" e as coloca na pasta "dist/images"</p>
+<p>Minifica o arquivo CSS em quanto você digita!</p>
+<p>Minifica o arquivo JS em quanto você digita!</p>
+<p>Pastas seguindo a estrutura de um projeto do mundo real!</p>
---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+// Initialize modules
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
+const imagemin = require('gulp-imagemin'); // npm i gulp-imagemin@7.1.0
+var cache = require('gulp-cache');
+const browsersync = require('browser-sync').create();
+
+
+// Minify images inside "app/images" folder and put the minified images inside "dist" folder
+function minifyImage(cb) {
+  src('app/images/*').pipe(imagemin({verbose: true})).pipe(dest('dist/images'));
+  cb();
+}
+
+
+//Cache clean task
+function clearCache(cb) {
+	return cache.clearAll();
+}
+
+// Sass Task
+function scssTask() {
+	return src('app/scss/style.scss', { sourcemaps: true })
+		.pipe(sass())
+		.pipe(postcss([autoprefixer(), cssnano()]))
+		.pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// JavaScript Task
+function jsTask() {
+	return src('app/js/script.js', { sourcemaps: true })
+		.pipe(babel({ presets: ['@babel/preset-env'] }))
+		.pipe(terser())
+		.pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+// Browsersync
+function browserSyncServe(cb) {
+	browsersync.init({
+		server: {
+			baseDir: '.',
+		},
+		notify: {
+			styles: {
+				top: 'auto',
+				bottom: '0',
+			},
+		},
+	});
+	cb();
+}
+function browserSyncReload(cb) {
+	browsersync.reload();
+	cb();
+}
+
+// Watch Task
+function watchTask() {
+	watch('*.html', browserSyncReload);
+	watch(
+		['app/scss/**/*.scss', 'app/**/*.js'],
+		series(scssTask, jsTask, browserSyncReload)
+	);
+}
+
+// Default Gulp Task
+
+//Descomente o código abaixo para minificar as imagens, é recomendado usar somente 1 vez e aguardar que otimize as imagens
+//exports.default = series(clearCache, scssTask, jsTask, minifyImage, browserSyncServe, watchTask);
+
+//Comente este código se for minificar a imagem, caso o contrário descomente.
+exports.default = series(clearCache, scssTask, jsTask, browserSyncServe, watchTask);
